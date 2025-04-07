0b7f8fd2559967acd230206d3e76d0ea5ce825e9
Kevin Wong <kevinwong973@gmail.com>
2025-04-07 12:33:37 -0700

    removed sensitive info firebase

--- frontend-web/src/lib/firebase.ts
+++ frontend-web/src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA62vywO5_Fc9SP-oHwYEsVGr1_XHJOpic",
-  authDomain: "caughtup-2b852.firebaseapp.com",
-  projectId: "caughtup-2b852",
-  storageBucket: "caughtup-2b852.firebasestorage.app",
-  messagingSenderId: "578117997464",
-  appId: "1:578117997464:web:7b7f52eef1a6d7650711ed",
-  measurementId: "G-R114JDHTW0"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
